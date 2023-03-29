8ccbdc8c1c88b3ab52d70f4450cd22fc2c18a3af
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-29 18:48:25 +0200

    add: function verifyRandomNumber

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,15 @@ const btnPlay = document.getElementById("btn");
 
 const container = document.querySelector(".container");
 
+const blackList = [];
+
+let randomNumber;
+
+do{
+  randomNumber = verifyRandomNumber(blackList);
+  blackList.push(randomNumber)
+}while(blackList.length !== 100)
+
 btnPlay.addEventListener("click", function(){
   container.classList.add("fade-in");
 
@@ -20,5 +29,13 @@ function createElement(){
 function getRandomNumber(){
   const number = Math.floor(Math.random() * 100) + 1;
   console.log(number);
+  return number;
+}
+
+function verifyRandomNumber(array){
+  let number;
+  do{
+    number = getRandomNumber();
+  }while(array.includes(number))
   return number;
 }
